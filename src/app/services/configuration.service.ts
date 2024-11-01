de77b7e91391ae4a527ae4c0c6ae5c413a32f595
idanmika <idanmi25@gmail.com>
2024-11-01 11:38:20 -0400

    Added Puppeteer

--- src/app/services/configuration.service.ts
+++ src/app/services/configuration.service.ts
@@ -1,29 +1,35 @@
 import puppeteer from 'puppeteer';
 // Or import puppeteer from 'puppeteer-core';
 
-// Launch the browser and open a new blank page
+  // Launch the browser and open a new blank page
 const browser = await puppeteer.launch();
 const page = await browser.newPage();
 
-// Navigate the page to a URL.
-await page.goto('https://developer.chrome.com/');
+export class ConfigurationService{
+  gatherWebData(path: string){
+    // Navigate the page to a URL.
+    page.goto('https://developer.chrome.com/');
 
-// Set screen size.
-await page.setViewport({width: 1080, height: 1024});
+    // Set screen size.
+    page.setViewport({width: 1080, height: 1024});
 
-// Type into search box.
-await page.locator('.devsite-search-field').fill('automate beyond recorder');
+    // Type into search box.
+    page.locator('.devsite-search-field').fill('automate beyond recorder');
 
-// Wait and click on first result.
-await page.locator('.devsite-result-item-link').click();
+    // Wait and click on first result.
+    page.locator('.devsite-result-item-link').click();
 
-// Locate the full title with a unique string.
-const textSelector = await page
-  .locator('text/Customize and automate')
-  .waitHandle();
-const fullTitle = await textSelector?.evaluate(el => el.textContent);
+    // Locate the full title with a unique string.
+    textSelector = page
+      .locator('text/Customize and automate')
+      .waitHandle();
+    fullTitle = textSelector?.evaluate(el => el.textContent);
 
-// Print the full title.
-console.log('The title of this blog post is "%s".', fullTitle);
+    // Print the full title.
+    console.log('The title of this blog post is "%s".', fullTitle);
 
-await browser.close();
+    browser.close();
+    }
+  
+}
+  
